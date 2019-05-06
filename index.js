01ae7ca7d29240117c1bcb713e508be1c471f601
Elliot Zoerner <eliot.zoerner@gmail.com>
2019-05-06 09:48:07 +0930

    Only adding new data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 import { runCron } from './lib/scraper'
-// import './lib/cron' // runs cron tasks
+import './lib/cron' // runs cron tasks
 
 
 // Get app secrets
@@ -29,8 +29,7 @@ mongoose.set('useFindAndModify', false);
 const Donations = require('./models/donations-model')
 
 app.get('/api/scrape', async (req, res, next) => {
-  runCron()
-  res.json({ success: true })
+  runCron(res)
 })
 
 app.get('/api/data', async (req, res, next) => {
@@ -46,6 +45,46 @@ app.get('/api/data', async (req, res, next) => {
     }
   )
 })
+
+/* app.get('/api/clean', async (req, res, next) => {
+  // Delete the useless scrapes
+  Donations.findOne(
+    { id: "1" },
+    (err, data) => {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        const dataObj = data.toObject()
+        const scrapes = dataObj.donations
+        const filteredScrapes = []
+        let prevScrape = null
+        scrapes.forEach(scrape => {
+          if (!prevScrape ||
+            JSON.stringify(prevScrape.people) !== JSON.stringify(scrape.people)) {
+            // Either the scrape is the first one
+            // Or there are changes since the last scrape
+            // So add it to the filtered scrapes
+            filteredScrapes.push(scrape)
+          }
+          prevScrape = scrape
+        })
+
+        Donations.findOneAndUpdate(
+          { id: "1" },
+          { $set: { donations: filteredScrapes } },
+          (err, data) => {
+            if (err) {
+              console.log(`Error: ${err}`);
+              res.json({ error: err })
+            } else {
+              res.json({ success: true })
+            }
+          }
+        )
+      }
+    }
+  )
+}) */
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`40 Hour Jammin Scraper running on http://localhost:${port}`))
